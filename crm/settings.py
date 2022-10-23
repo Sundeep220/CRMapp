cf0ed914585c4e310374550cde1a0f40a26e27cd
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-10-23 19:24:57 +0530

    Added configurations to urls to acces media using media url

--- crm/settings.py
+++ crm/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'django_extensions'
+    'django_extensions',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
