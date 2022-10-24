4f98ced4341994b217e436b1a92c43393c3060d6
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-10-24 14:25:53 +0530

    Added styling to reset password pages

--- crm/settings.py
+++ crm/settings.py
@@ -79,9 +79,18 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO TEST',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
+    #This is configuration for sqlite db
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
